63dba201b3f0648ed4e27d97e9c3d8886dd6dbd7
Jonathan <2236000300@utna.edu.mx>
2026-08-30 19:05:49 -0600

    Probando despliegue1

--- server/config/env.js
+++ server/config/env.js
@@ -12,7 +12,7 @@ function parseCorsOrigins(value) {
 export const env = {
   nodeEnv: process.env.NODE_ENV || 'development',
   port: Number(process.env.PORT) || 4000,
-  mongoUri: process.env.MONGO_URI || 'mongodb+srv://2236000300_db_user:<BRLzrz8lza00zWAf>@xv-allison.ykh4ndy.mongodb.net/?appName=xv-allison',
+  mongoUri: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/xv_allison',
   jwtSecret: process.env.JWT_SECRET || 'dev_change_this_secret',
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || '8h',
   corsOrigins: parseCorsOrigins(process.env.CORS_ORIGIN || 'http://localhost:5173'),
